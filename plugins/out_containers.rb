d93cb75f17cd0235038987e5e49ad0cad0a8a937
Karl Stoney <kstoney@thoughtworks.com>
2016-09-09 09:44:32 +0100

    More awesome tags for docker

--- plugins/out_containers.rb
+++ plugins/out_containers.rb
@@ -16,10 +16,24 @@ module Fluent
 
     def emit(tag, es, chain)
       # tag example: containers.mnt.log.containers.rabbitmq-2862264727-393n0_td-integration_master-7c9d336c240d2238c8030c06310a2a0579d69d906f73ff06731373b1f820b99b.log
-      tag = tag.split('.')[4].split('-')[0..-2].join('-')
+      parts = tag.split('_')
+      # gives you: ["containers.mnt.log.containers.rabbitmq-2862264727-393n0", "td-integration", "master-7c9d336c240d2238c8030c06310a2a0579d69d906f73ff06731373b1f820b99b.log"]
+      pod = parts[0].split('.').last
+      namespace = parts[1]
+      container_name = parts[2].split('-').first
+      replica_set = pod.split('-')[0..-2].join('-')
+
       es.each do |time, record|
         record['log'] = record['log'].strip
-        router.emit(tag, time, record)
+        data = {
+          'namespace' => namespace,
+          'container_name' => container_name,
+          'replica_set' => replica_set
+        }.merge(record)
+        if not record['log'] == '' then
+          # We only care about non-empty logs
+          router.emit(tag, time, data)
+        end
       end
 
       chain.next
