b56e8ba34ebb0afffc33c50355a857126eb072e9
Karl Stoney <kstoney@thoughtworks.com>
2016-09-09 09:20:09 +0100

    Getting bored of writing commits now but this is the only way to get a feedback loop as I dont have kubernetes locally

--- plugins/out_containers.rb
+++ plugins/out_containers.rb
@@ -5,17 +5,21 @@ module Fluent
     # Register type
     Fluent::Plugin.register_output('container_format', self)
 
+    # To support Fluentd v0.10.57 or earlier
+    unless method_defined?(:router)
+      define_method("router") { Fluent::Engine }
+    end
+
     def configure(conf)
       super
     end
 
     def emit(tag, es, chain)
-      $stdout.puts tag
-      #tag = kubernetes.${tag_suffix[4].split('-')[0..-2].join('-')}
+      # tag example: containers.mnt.log.containers.rabbitmq-2862264727-393n0_td-integration_master-7c9d336c240d2238c8030c06310a2a0579d69d906f73ff06731373b1f820b99b.log
+      tag = tag.split('.')[4].split('-')[0..-2].join('-')
       es.each do |time, record|
         record['log'] = record['log'].strip
-        $stdout.puts record
-        Engine.emit(tag, time, record)
+        router.emit(tag, time, record)
       end
 
       chain.next
